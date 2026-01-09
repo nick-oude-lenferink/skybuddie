9e2f197ff4b726e60be096b4566a42ad8ddfff12
Nick Oude Lenferink <noudelenferink@gmail.com>
2026-01-09 10:49:19 +0100

    Added map

---
+++ src/app/map/map.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {LeafletDirective} from '@bluehalo/ngx-leaflet'
+import { latLng, tileLayer } from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.page.html',
+  styleUrls: ['./map.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, LeafletDirective]
+})
+export class MapPage implements OnInit {
+options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      })
+    ],
+    zoom: 10,
+    center: latLng(52.0705, 4.3007) // Den Haag
+  };
+  constructor() { }
+ 
+  ngOnInit() {
+    
+  }
+
+}
